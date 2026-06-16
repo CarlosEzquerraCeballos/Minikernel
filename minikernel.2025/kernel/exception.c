d976e95b0ccb372ecfb0bea5c9f9878636d83445
CarlosEzquerraCeballos <carlosezquerraceballos@gmail.com>
2026-06-16 21:22:58 +0200

    proyecto base

---
+++ minikernel.2025/kernel/exception.c
@@ -0,0 +1,17 @@
+/*
+ *  kernel/exception.c
+ *
+ *  Minikernel (versión 2.0)
+ *
+ *  Fernando Pérez Costoya
+ *
+ */
+
+#include "HAL.h"
+#include "exception.h"
+#include "common.h"
+
+/* Funciones relacionadas con el tratamiento de excepciones */
+
+void init_exception_module(void) {
+}
